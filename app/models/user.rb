f99ca8228538f5ca437c2d88f7f255683652f113
claudiob <claudiob@gmail.com>
2013-05-11 12:10:09 -0700

    Set Feeling dependent: :destroy to user/word

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :feelings, dependent: :destroy
   def to_s
     name
   end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ActiveRecord::Base
+  has_many :feelings, dependent: :destroy
   validates :text, presence: true, uniqueness: true, length: {maximum: 64} # for a faster index
   def to_s
     text
